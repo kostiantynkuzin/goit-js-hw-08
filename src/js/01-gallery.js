d638624cca6815383b6e2d88f45537ffa88742a2
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-23 17:28:15 +0300

    01-finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <div class="gallary__item">
+        <a class= "gallery__link" href="${original}">
+          <img 
+            class= "gallery__image"
+            src="${preview}" 
+            data-source="${original}" 
+            alt="${description}"
+          />
+        </a>
+    </div>
+  `;
+    })
+    .join('');
+}
+const containerGallery = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryCards(galleryItems);
+
+containerGallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// containerGallery.addEventListener(clickOnGalleryCards);
+
+// function clickOnGalleryCards(evt) {
+//   // console.log(evt.target);
+//   // if (!evt.target.classList.contains("gallery__image")) {
+//   //   return;
+//   // }
+//   evt.preventDefault();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+lightbox.show();
+// }
+console.log(lightbox.show());
