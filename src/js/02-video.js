62154daef6b371d89aa98d4fe8e871b94e55dfa2
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-24 20:55:29 +0300

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+// const video = document.getElementById('vimeo-player');
+const iframePlayer = new Player(iframe);
+// console.log(video);
+
+// iframePlayer.on('play', function () {
+//   console.log('played the video!');
+// });
+
+const currentTimeOn = 'videoplayer-current-time';
+iframePlayer.on('timeupdate', function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+});
+
+// iframePlayer.setCurrentTime(currentTimeOn).then(function (100) {
+
+// });
